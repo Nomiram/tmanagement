df36b76bbfa90883a23c9c478e7c3ebb70bfc955
Мироненко Андрей <mironenko.av@nppgamma.ru>
2023-12-24 02:46:56 +0300

    move init to file

---
+++ tmanagement/init.sql
@@ -0,0 +1,34 @@
+	CREATE TABLE IF NOT EXISTS public.orders
+(
+    order_name character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    start_date date,
+    CONSTRAINT orders_pkey PRIMARY KEY (order_name)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.orders
+    OWNER to postgres;
+
+-- Table: public.tasks
+
+-- DROP TABLE IF EXISTS public.tasks;
+
+CREATE TABLE IF NOT EXISTS public.tasks
+(
+    task character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    order_name character varying(10) COLLATE pg_catalog."default",
+    duration integer,
+    resource integer,
+    pred character varying(10) COLLATE pg_catalog."default",
+    CONSTRAINT tasks_pkey PRIMARY KEY (task, order_name),
+    CONSTRAINT tasks_order_name_fkey FOREIGN KEY (order_name)
+        REFERENCES public.orders (order_name) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.tasks
+    OWNER to postgres;
--- tmanagement/server.go
+++ tmanagement/server.go
@@ -78,41 +78,12 @@ func checkDB() bool {
 	if err != nil {
 		panic(err)
 	}
-	_, err = db.Query(`
-	CREATE TABLE IF NOT EXISTS public.orders
-(
-    order_name character varying(10) COLLATE pg_catalog."default" NOT NULL,
-    start_date date,
-    CONSTRAINT orders_pkey PRIMARY KEY (order_name)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.orders
-    OWNER to postgres;
-
--- Table: public.tasks
-
--- DROP TABLE IF EXISTS public.tasks;
-
-CREATE TABLE IF NOT EXISTS public.tasks
-(
-    task character varying(10) COLLATE pg_catalog."default" NOT NULL,
-    order_name character varying(10) COLLATE pg_catalog."default",
-    duration integer,
-    resource integer,
-    pred character varying(10) COLLATE pg_catalog."default",
-    CONSTRAINT tasks_pkey PRIMARY KEY (task, order_name),
-    CONSTRAINT tasks_order_name_fkey FOREIGN KEY (order_name)
-        REFERENCES public.orders (order_name) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.tasks
-    OWNER to postgres;`)
+	b, err := os.ReadFile("init.sql")
+	if err != nil {
+		fmt.Print(err)
+	}
+	str := string(b)
+	_, err = db.Query(str)
 	if err != nil {
 		panic(err)
 	}
